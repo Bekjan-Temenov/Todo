25dd8fe9eb0cc9984803eaa52252757fbc901389
Bekjan <temenovbekzan@gmail.com>
2024-06-04 18:58:10 +0600

    todo

--- src/shared/ui/Container.jsx
+++ src/shared/ui/Container.jsx
@@ -1,6 +1,6 @@
 export function Container({ children }) {
 	return (
-		<div className='w-[360px] sm:w-[680px] md:w-[800px] lg:w-[1200px] m-auto  '>
+		<div className='w-[360px] sm:w-[680px] md:w-[800px] lg:w-[800px] m-auto  '>
 			{children}
 		</div>
 	)
